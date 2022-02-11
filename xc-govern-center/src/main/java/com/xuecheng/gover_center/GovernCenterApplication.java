33ff1c4a1853d645936d11ded2f96235dc15997d
heyz <heyz3150@foxmail.com>
2022-02-11 14:53:29 +0800

    jwt登录验证 zuul网关

--- xc-govern-center/src/main/java/com/xuecheng/gover_center/GovernCenterApplication.java
+++ xc-govern-center/src/main/java/com/xuecheng/gover_center/GovernCenterApplication.java
@@ -3,10 +3,12 @@ package com.xuecheng.gover_center;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 
 @EnableEurekaServer
 @SpringBootApplication
+@EnableZuulProxy
 public class GovernCenterApplication {
 
     public static void main(String[] args) {
